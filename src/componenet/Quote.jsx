375659130caeeb45f1a31960d760b8ebfb998de9
ramesh kumar mahato <rameshsingh9813@gmail.com>
2023-03-21 09:35:35 +0545

    writen by self

--- src/componenet/Quote.jsx
+++ src/componenet/Quote.jsx
@@ -5,19 +5,20 @@ function Quote() {
   const [quote, setQuote] = useState({ text: "", author: "" });
   const [allQuotes, setAllQuotes] = useState([]);
 
-  console.log(allQuotes);
-  // Fetch all quotes on initial render
-  useEffect(() => {
-    axios
-      .get("https://type.fit/api/quotes")
-      .then((res) => {
-        setAllQuotes(res.data)
-    }
-      )
-      .catch((err) => console.error(err));
-  }, []);
 
-  console.log(allQuotes)
+//  quote api   https://type.fit/api/quotes
+  // Fetch all quotes on initial render
+     useEffect(()=>{
+        axios
+        .get("https://type.fit/api/quotes")
+        .then((response)=>{
+            setAllQuotes(response.data);
+        })
+        .catch((error)=>console.log(error))
+        },[])
+
+     
+   
 
   // Fetch a random quote from all quotes
   function getQuote() {
@@ -29,11 +30,13 @@ function Quote() {
   }
 
   return (
-    <div className="App">
+    <div className="App card"style={{"width": "50rem","margin":"200px auto 200px auto","backgroundColor":"grey","padding":"30px","borderRadius":"10px","color":"white"}}>
+        <div class="card-body">
       <p><b>"{quote.text}"</b></p>
       <p><i>-{quote.author}</i></p>
 
-      <button onClick={getQuote}>New quote</button>
+      <button onClick={getQuote} className="btn btn-primary">New quote</button>
+      </div>
     </div>
   );
 }
